8711f8ea519f528604bbb424a7f6d65d72fb1f34
Brian Williams <heffergod@gmail.com>
2022-09-09 22:25:33 -0700

    finished adding first feature to assessment codebase

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ["You will step on the soil of many countries!", "You shall come across a bathroom the moment you need one!", "A million dollars is gonna fall right from the frickin' sky, I swear!", "Right now, someone nice is thinking something nice about you, and that's just swell!", "You deserve everything coming to you!"];
+
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
     }
 
 }
